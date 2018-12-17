a42d5d56086628c5f7400d5ece0bcba13966c1cd
tomeraitz <tomeraitz1@gmail.com>
2018-12-17 15:46:45 +0200

    finshed thr homework

--- server.js
+++ server.js
@@ -17,121 +17,121 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-/*=====================================================
-Create books Collection
-=======================================================*/
-var isbns = [9780156012195, 9780743273565, 9780435905484, 9780140275360, 9780756404741, 9780756407919, 9780140177398, 9780316769488, 9780062225672, 9780143130154, 9780307455925, 9781501143519]
-var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+// /*=====================================================
+// Create books Collection
+// =======================================================*/
+// var isbns = [9780156012195, 9780743273565, 9780435905484, 9780140275360, 9780756404741, 9780756407919, 9780140177398, 9780316769488, 9780062225672, 9780143130154, 9780307455925, 9781501143519]
+// var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
 
-for (var i = 0; i < isbns.length; i++) {
-  var apiURL = url + isbns[i];
-  /*=====================================================
-  the first time you run your code, uncomment the function below.
-  for subsequent runs, re-comment it so that it runs only once!
-  that said, there is a fail-safe to avoid duplicates below  
-  =======================================================*/
-  loadFromAPI(apiURL)
-}
-console.log("done");
+// for (var i = 0; i < isbns.length; i++) {
+//   var apiURL = url + isbns[i];
+// //   /*=====================================================
+// //   the first time you run your code, uncomment the function below.
+// //   for subsequent runs, re-comment it so that it runs only once!
+// //   that said, there is a fail-safe to avoid duplicates below  
+// //   =======================================================*/
+//   loadFromAPI(apiURL)
+// }
+// console.log("done");
 
-function loadFromAPI(apiURL) {
+// function loadFromAPI(apiURL) {
 
-  request(apiURL, function(error, response, body) {
+//   request(apiURL, function(error, response, body) {
 
-    var result = JSON.parse(body)
+//     var result = JSON.parse(body)
 
-    if (result.totalItems && !error && response.statusCode == 200) {
-      var resBook = JSON.parse(body).items[0].volumeInfo
+//     if (result.totalItems && !error && response.statusCode == 200) {
+//       var resBook = JSON.parse(body).items[0].volumeInfo
 
-      var book = new Book({
-        title: resBook.title,
-        author: resBook.authors[0],
-        pages: resBook.pageCount,
-        genres: resBook.categories || ["Other"],
-        rating: resBook.averageRating || 5
-      })
+//       var book = new Book({
+//         title: resBook.title,
+//         author: resBook.authors[0],
+//         pages: resBook.pageCount,
+//         genres: resBook.categories || ["Other"],
+//         rating: resBook.averageRating || 5
+//       })
 
-      //Only save if the book doesn't exist yet
-      Book.findOne({ title: book.title }, function(err, foundBook) {
-        if (!foundBook) {
-          book.save()
-        }
-      })
-    }
-  })
-}
-
-
-/*=====================================================
-Create People Collection
-=======================================================*/
-var colors = ["brown", "black", "red", "yellow", "green", "grey"]
-var getColor = function() {
-  return colors[Math.floor(Math.random() * colors.length)]
-}
-var getWeight = function() {
-  return getRandIntBetween(50, 120)
-}
-var getHeight = function() {
-  return getRandIntBetween(120, 230)
-}
-var getSalary = function() {
-  return getRandIntBetween(20000, 50000)
-}
-var getNumKids = function() {
-  return Math.floor(Math.random() * 3)
-}
-
-var getRandIntBetween = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-var getKids = function(numKids) {
-  var kids = [];
-  for (var i = 0; i < numKids; i++) {
-    kids.push({
-      hair: getColor(),
-      eyes: getColor(),
-      weight: getWeight(),
-      height: getHeight(),
-    })
-  }
-  return kids;
-}
+//       //Only save if the book doesn't exist yet
+//       Book.findOne({ title: book.title }, function(err, foundBook) {
+//         if (!foundBook) {
+//           book.save()
+//         }
+//       })
+//     }
+//   })
+// }
 
 
-/*=====================================================
-the below code always makes sure
-you don't have over 100 people and 
-adds new people and their kids until you do have 100
+// // /*=====================================================
+// // Create People Collection
+// // =======================================================*/
+// var colors = ["brown", "black", "red", "yellow", "green", "grey"]
+// var getColor = function() {
+//   return colors[Math.floor(Math.random() * colors.length)]
+// }
+// var getWeight = function() {
+//   return getRandIntBetween(50, 120)
+// }
+// var getHeight = function() {
+//   return getRandIntBetween(120, 230)
+// }
+// var getSalary = function() {
+//   return getRandIntBetween(20000, 50000)
+// }
+// var getNumKids = function() {
+//   return Math.floor(Math.random() * 3)
+// }
 
-try to understand how this code works
-could you write it differently?
-=======================================================*/
-Person.find({}).count(function(err, count) {
-  // the below two loops could be changed to a simple:
-  // for (var i = count; i < 100; i++) {}
-  if (count < 100) {
-    for (var i = 0; i < 100 - count; i++) {
-      var numKids = getNumKids();
-      var p = new Person({
-        hair: getColor(),
-        eyes: getColor(),
-        weight: getWeight(),
-        height: getHeight(),
-        salary: getSalary(),
-        numKids: numKids,
-        kids: getKids(numKids)
-      });
-      p.save();
-    }
-  }
-})
+// var getRandIntBetween = function(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min)
+// }
+
+// var getKids = function(numKids) {
+//   var kids = [];
+//   for (var i = 0; i < numKids; i++) {
+//     kids.push({
+//       hair: getColor(),
+//       eyes: getColor(),
+//       weight: getWeight(),
+//       height: getHeight(),
+//     })
+//   }
+//   return kids;
+// }
 
 
-/*=====================================================
-Start the server:
-=======================================================*/
+// // /*=====================================================
+// // the below code always makes sure
+// // you don't have over 100 people and 
+// // adds new people and their kids until you do have 100
+
+// // try to understand how this code works
+// // could you write it differently?
+// // =======================================================*/
+// Person.find({}).count(function(err, count) {
+//   // the below two loops could be changed to a simple:
+//   // for (var i = count; i < 100; i++) {}
+//   if (count < 100) {
+//     for (var i = 0; i < 100 - count; i++) {
+//       var numKids = getNumKids();
+//       var p = new Person({
+//         hair: getColor(),
+//         eyes: getColor(),
+//         weight: getWeight(),
+//         height: getHeight(),
+//         salary: getSalary(),
+//         numKids: numKids,
+//         kids: getKids(numKids)
+//       });
+//       p.save();
+//     }
+//   }
+// })
+
+
+// /*=====================================================
+// Start the server:
+// =======================================================*/
 
 app.listen(3000, function() {
   console.log("Server up and running on port 3000")
@@ -151,19 +151,68 @@ and your server is running do the following:
 ----------------------*/
 //1. Find books with fewer than 500 but more than 200 pages
 
+// Book.find({$and : [
+//                     {pages : {$gt : 200}},
+//                     {pages :{$lt : 500}}
+//                   ]}, 
+// function(err, book){
+//     console.log(book)
+// })
+
 //2. Find books whose rating is less than 5, and sort by the author's name
+
+// Book.find({rating : {$lt : 5}}).sort({author : -1}).exec(function(error, books){
+//   console.log(books)
+// });
 
 //3. Find all the Fiction books, skip the first 2, and display only 3 of them 
 
+// Book.find({$or : [{genres : "Fiction"},{genres : "fiction"}]}).skip(2).limit(2).exec( function(err, books){
+//   console.log(books)
+// })
 
 /*People
 ----------------------*/
 //1. Find all the people who are tall (>180) AND rich (>30000)
 
+// Person.find({$and : [{height : {$gt : 180}}, {salary : {$gt : 3000}}]},function(err, people){
+//     console.log(people)
+// })
+
 //2. Find all the people who are tall (>180) OR rich (>30000)
 
+// Person.find({$or : [{height : {$gt : 180}}, {salary : {$gt : 3000}}]}, function(err, people){
+//   console.log(people)
+// })
+
 //3. Find all the people who have grey hair or eyes, and are skinny (<70)
+// Person.find({$and : [
+//   {$or : [
+//     {hair : "grey"},
+//     {eyes : "grey"}
+//   ]},
+//   {weight : {$lt : 70}}
+// ]}, function(err, people){
+//     console.log(people)
+//   })
 
 //4. Find people who have at least 1 kid with grey hair
 
+// Person.find({$and : [
+//   {numKids : {$gte : 1}},
+//   {"kids.hair" : "grey"}
+// ]}, function(err, people){
+//       console.log(people)
+//     })
+
 //5. Find all the people who have at least one overweight kid, and are overweight themselves (>100)
+
+// Person.find({$and :[
+//   {kids : {$elemMatch : {weight : {$gt: 100}}}},
+//   {weight : {$gt :100}}
+// ]}).exec( function(err, people){
+//       people.forEach(p => {
+//         console.log(p)
+//       })
+//       console.log(people.length)
+//   })
